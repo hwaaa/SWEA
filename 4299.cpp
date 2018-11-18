7605c80a334aed974bbd1c8ee6d40d5e5507f115
hwaaa <hshwaa@gmail.com>
2018-11-18 22:47:10 +0900

    재혁이의 사랑은 타이밍

---
+++ 4299.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+// 1시간 = 60분
+// 24시간 = 1440분
+
+int main() {
+	int test_case;
+	int T;
+	int pprday = 11;
+	int pprhour = 11;
+	int pprminute = 11;
+	int day = 0;
+	int hour = 0;
+	int minute = 0;
+	int result;
+	cin >> T;
+
+	for (test_case = 1; test_case <= T; ++test_case) {
+		
+		cin >> day >> hour >> minute;
+		
+		if (pprday == day && pprhour > hour) {
+			//result = -1;
+			cout << "#" << test_case << " " << -1;
+		}
+		else {
+			day -= pprday;
+			day *= 1440;
+			hour -= pprhour;
+			hour *= 60;
+			minute -= pprminute;
+
+			result = day + hour + minute;
+			cout << "#" << test_case << " " << result;
+			
+		}
+				
+		cout << endl;
+	}
+}
